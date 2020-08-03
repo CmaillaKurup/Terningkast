d6ceb36032c8fecbc7e50fc6ee8467fec7e1c69d
cami670v <cami670v@ZBC-AA-UBUY12>
2020-08-03 13:43:50 +0200

    Add project files.

---
+++ Terningkast/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Terningkast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.BackgroundColor = ConsoleColor.DarkGray;
+            Random random = new Random();
+            int terningskast = random.Next(1, 7);
+
+            switch(terningskast)
+            {
+                case 1:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Du slog en 1'er");
+                    break;
+                case 2:
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine("Du slog en 2'er");
+                    break;
+                case 3:
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine("Du slog en 3'er");
+                    break;
+                case 4:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Du slog en 4'er");
+                    break;
+                case 5:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Du slog en 5'er");
+                    break;
+                default:
+                    Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                    Console.WriteLine("Du slog en 6'er");
+                    break;
+            }
+        }
+    }
+}
